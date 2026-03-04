7e62b391cc2ef5bc97a7993af631fca366b0d0ee
Nitin Mohapatra <nitinmohapatra26@gmail.com>
2026-03-04 16:48:35 +0530

    done

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path');
 const port = 8080;
 const gmbRoutes = require('./routes/gmbRoutes');
-// const { json } = require('stream/consumers');
 const cors = require('cors');
 
 app.use(cors({
